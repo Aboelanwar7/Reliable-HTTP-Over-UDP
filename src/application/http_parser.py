8b8609bdc38554cd7947cb34b11d9fccd41a376e
Yousseff-Mohammed <ymohammedjoe1@gmail.com>
2026-04-25 23:03:54 +0300

    Fix HTTP Parser Typos and Logic Errors

--- src/application/http_parser.py
+++ src/application/http_parser.py
@@ -2,7 +2,7 @@ class HTTPMessage:
     def __init__(self,):
         self.method = None
         self.path = None
-        self.versin = "HTTP/1.0"
+        self.version = "HTTP/1.0"
         self.status_code = None
         self.status_phrase = None
         self.headers = {}
@@ -15,16 +15,16 @@ class HTTPParser:
         if headers is None:
             headers = {}
 
-            request = f"{method} {path} HTTP/1.0\r\n"
+        request = f"{method} {path} HTTP/1.0\r\n"
 
-            if body and "Content-Length" not in headers:
-                headers["Content-Length"] = str(len(body))
+        if body and "Content-Length" not in headers:
+            headers["Content-Length"] = str(len(body.encode('utf-8')))
 
-            for key, value in headers.items():
-                request += f"{key}: {value}\r\n"
+        for key, value in headers.items():
+            request += f"{key}: {value}\r\n"
 
-            request += "\r\n" + body
-            return request
+        request += "\r\n" + body
+        return request.encode('utf-8')
 
     @staticmethod
     def build_response(status_code, status_phrase, body="", headers=None):
@@ -41,14 +41,18 @@ class HTTPParser:
             response += f"{key}: {value}\r\n"
 
         response += "\r\n" + body
-        return response
+        return response.encode('utf-8')
 
     @staticmethod
     def parse(raw_data):
         """Parses a raw HTTP string into an HTTPMessage object."""
+        # Ensure we are working with a string
+        if isinstance(raw_data, bytes):
+            raw_data = raw_data.decode('utf-8', errors='ignore')
+
         # HTTP headers and body are separated by a double CRLF
         parts = raw_data.split("\r\n\r\n", 1)
-        header_secrion = parts[0]
+        header_section = parts[0]
         body = parts[1] if len(parts) > 1 else ""
 
         lines = header_section.split("\r\n")
@@ -61,8 +65,8 @@ class HTTPParser:
         if start_line[0].startswith("HTTP"):
             # It's a Response
             msg.version = start_line[0]
-            msg.status_code = start_line[1]
-            msg.status_phrase = start_line[2]
+            msg.status_code = int(start_line[1])
+            msg.status_phrase = " ".join(start_line[2:])
         else:
             # It's a Request
             msg.method = start_line[0]
@@ -73,6 +77,6 @@ class HTTPParser:
         for line in lines[1:]:
             if ": " in line:
                 key, value = line.split(": ", 1)
-                msg.header[key] = value
+                msg.headers[key] = value
 
         return msg
